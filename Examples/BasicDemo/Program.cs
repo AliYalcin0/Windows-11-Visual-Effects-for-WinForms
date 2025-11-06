f492ac6179a9ac52e40595d56d32205b7861a99e
Oxgen <ylcnali08@gmail.com>
2025-11-06 20:41:20 +0300

    Add Program.cs for Windows 11 effects demo

---
+++ Examples/BasicDemo/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Windows.Forms;
+
+namespace Windows11Effects.Demo
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            // Check Windows version and show appropriate demo
+            if (Windows11Effects.IsWindows11OrGreater())
+            {
+                Application.Run(new AdvancedExampleForm());
+            }
+            else
+            {
+                Application.Run(new BasicExampleForm());
+            }
+        }
+    }
+}
